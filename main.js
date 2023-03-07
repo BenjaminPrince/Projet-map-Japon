e218c3c3d4c9b7c30ea7a3293c0a2f4afcea7492
BenjaminPrince <prince-benjamin@hotmail.com>
2023-03-07 17:12:30 +0100

    maj

--- main.js
+++ main.js
@@ -6,15 +6,11 @@ let mymap = L.map('mapid', {
     attributionControl: false
 });
 
-  // Ajout du fond de carte OpenStreetMap
-  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data © <a href="https://www.mapbox.com/">Mapbox</a>',
+// Ajout du fond de carte OpenStreetMap
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1Ijoic3Vla2swIiwiYSI6ImNsZXllcmFjeTA0OHUzd3B1YWJldWJjNHQifQ.NbGBkm9RSND7-X17LaPsKw',
-    }).addTo(mymap);
+}).addTo(mymap);
 
 // Ajout d'un marqueur pour Tokyo
 L.marker([35.6828, 139.759]).addTo(mymap)
